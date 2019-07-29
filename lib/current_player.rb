5af110343c5fb68c9f86c8f58f4a856d1186e12c
Cristian Rogelio Garcia Valdez <eminemko@hotmail.com>
2019-07-29 17:19:50 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  turn = 0
+  board.each{ |index|
+    if index =="X" || index=="O"
+      turn+=1
+    end    
+  }
+  return turn
+end
+
+def current_player(board)
+
+end
+
